2de591687bc28f91bd34c80522bda314e39ca1f2
Fernanda Ferreira <fefcufe@gmail.com>
2022-03-08 10:37:57 -0300

    Requisito 4 entregue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -33,6 +34,30 @@ class Header extends Component {
     if (loading) return <Loading />;
     return (
       <header data-testid="header-component">
+        <nav>
+          <ul id="links">
+            <li>
+              <Link
+                to="/search"
+                data-testid="link-to-search"
+              >
+                Search
+              </Link>
+              <Link
+                to="/favorites"
+                data-testid="link-to-favorites"
+              >
+                Favorites
+              </Link>
+              <Link
+                to="/profile"
+                data-testid="link-to-profile"
+              >
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </nav>
         <p data-testid="header-user-name"><strong>{ name }</strong></p>
       </header>
     );
